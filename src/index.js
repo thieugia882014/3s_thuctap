2acfec03a626cbe36c26f58b9ce3262a659cd525
thieugia882014 <70931357+thieugia882014@users.noreply.github.com>
2021-01-12 17:14:03 +0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import "./styles.css";
+//In ra các phần tử âm của mảng
+function negative(array) {
+  for (let i = 0; i < array.length; i++) {
+    const soAm = array[i];
+    if (soAm < 0) console.log(soAm);
+  }
+}
+negative([2, 1, -1, -2, 3]);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//Tách 1 mảng thành 2 mảng chăn và lẻ
+
+function chanLe(mang) {
+  let ketQua = {
+    mangChan: [],
+    mangLe: []
+  };
+  mang.forEach(function (item, index) {
+    if (item % 2 == 0) ketQua.mangChan.push(item);
+    else ketQua.mangLe.push(item);
+  });
+  return ketQua;
+}
+console.log(chanLe([1, 2, 3, 4, 5]));
+
+//Lấy ra các phần tử chẵn và nhỏ hơn 20
+function layMangChan(mang) {
+  let ketQua = [];
+  mang.forEach(function (item, index) {
+    if (item % 2 == 0 && item < 20) ketQua.push(item);
+  });
+  return ketQua;
+}
+console.log(layMangChan([1, 2, 3, 4, 5, 21, 22]));
